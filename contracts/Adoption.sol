52131c88d3d9cd77d19d65c63e1e051cfa303e8b
rogreenf <rogreenf@cisco.com>
2017-07-23 22:50:35 -0700

    First commit

---
+++ contracts/Adoption.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.11;
+
+contract Adoption {
+  address[16] public adopters;
+
+  function adopt(uint petId) public returns (uint) {
+    if (petId < 0 || petId > 15) {
+      throw;
+    }
+
+    adopters[petId] = msg.sender;
+
+    return petId;
+  }
+
+  function getAdopters() public returns (address[16]) {
+    return adopters;
+  }
+}
